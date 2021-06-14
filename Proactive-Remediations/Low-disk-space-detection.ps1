22fcae7a139547aa69c9f07b0d01447c0693a97e
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-06-14 14:29:40 +0100

    updating

--- Proactive-Remediations/Low-disk-space-detection.ps1
+++ Proactive-Remediations/Low-disk-space-detection.ps1
@@ -1,8 +1,10 @@
 $os = Get-CimInstance Win32_OperatingSystem
 $systemDrive = Get-CimInstance Win32_LogicalDisk -Filter "deviceid='$($os.SystemDrive)'"
-if (($systemDrive.FreeSpace/$systemDrive.Size) -le '0.10') {
-     return $false
+if (($systemDrive.FreeSpace/$systemDrive.Size) -le '0.70') {
+    Write-Output "Disk space is considered low. Script is exiting with 1 indicating error"
+    exit 1
 }
-else { 
-     return $true 
+else {
+    Write-Output "Disk space is OK."
+    exit 0
 }
--- Proactive-Remediations/Low-disk-space.ps1
+++
@@ -1,8 +0,0 @@
-$os = Get-CimInstance Win32_OperatingSystem
-$systemDrive = Get-CimInstance Win32_LogicalDisk -Filter "deviceid='$($os.SystemDrive)'"
-if (($systemDrive.FreeSpace/$systemDrive.Size) -le '0.10') {
-     return $false
-}
-else { 
-     return $true 
-}
