f8faf35f8fc8a8e05d5cbe011c3c117b3bf19afc
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-06-17 12:25:49 +0100

    Update Low-disk-space-detection.ps1

--- Proactive-Remediations/Low-disk-space-detection.ps1
+++ Proactive-Remediations/Low-disk-space-detection.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+    Low Disk Space Detection Script.
+
+.DESCRIPTION
+    Script to detect the amount of free space on the computer.
+    
+.PARAMETER Config
+    Specify the minimum amount of free space as a number e.g. 0.10 = 10% or 0.70 = 70%  
+    e.g.
+    if (($systemDrive.FreeSpace/$systemDrive.Size) -le '0.10')
+
+.NOTES
+    Filename: Low-disk-space-detection.ps1
+    Version: 1.1
+     
+    Version history:
+
+    1.1   -   Production version
+    1.0.1 -   Add Synopsis, Description, Paramenter, notes, and set minimum percentage of free space.
+    1.0   -   Script created
+
+.LINK
+    https://morethanpatches.com/
+    https://poweronplatforms.com
+#>
+
 $os = Get-CimInstance Win32_OperatingSystem
 $systemDrive = Get-CimInstance Win32_LogicalDisk -Filter "deviceid='$($os.SystemDrive)'"
 if (($systemDrive.FreeSpace/$systemDrive.Size) -le '0.10') {
