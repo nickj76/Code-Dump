c82aee5f11efd06be960ef696097e7e542b394b9
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-06-23 11:47:35 +0100

    updating

--- PowerShell Install Scripts/Install-Firefox-latest.ps1
+++ PowerShell Install Scripts/Install-Firefox-latest.ps1
@@ -24,7 +24,7 @@ if (Test-Path firefox-latest.exe -PathType leaf) {
 }
 else {
     Write-Host ">> Downloading Latest Version of Mozilla Firefox"
-    Invoke-WebRequest 'https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US' -OutFile firefox-latest.exe
+    Invoke-WebRequest 'https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-GB' -OutFile firefox-latest.exe
     Write-Host ">> Download Complete"
 }
 
