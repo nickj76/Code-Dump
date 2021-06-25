2fb309cbc67b1aa1518581a0ed46fab9b98bb24a
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-06-25 14:53:28 +0100

    Update Scheduled-Tasks-Monthly.ps1

--- Scheduled-Tasks-PS/Scheduled-Tasks-Monthly.ps1
+++ Scheduled-Tasks-PS/Scheduled-Tasks-Monthly.ps1
@@ -1,5 +1,5 @@
 $Trigger= New-ScheduledTaskTrigger -At 3:00pm -Weekly -WeeksInterval 4 -DaysOfWeek Thursday # Specify the trigger settings
 $User= "NT AUTHORITY\SYSTEM" # Specify the account to run the script
-$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "C:\PS\Windows-Install-Firefox-latest.ps1" # Specify what program/script to run and with its parameters
+$Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument -ExecutionPolicy Bypass -File "C:\PS\Windows-Install-Firefox-latest.ps1" # Specify what program/script to run and with its parameters
 Register-ScheduledTask -TaskName "Firefox Updater" -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force # Specify the name of the task
 New-ScheduledTaskSettingsSet -Compatibility V1 -AllowStartIfOnBatteries 
