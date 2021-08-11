b1428aed933d08215dacea382abd2179596cad72
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-08-11 17:05:28 +0100

    added to repo

---
+++ Kill Adobe processes/ka3.bat
@@ -0,0 +1,21 @@
+REM Kill Adobe processes
+taskkill.exe /F /FI "IMAGENAME eq acrotray*"
+taskkill.exe /F /FI "IMAGENAME eq acrobat_sl*"
+taskkill.exe /F /FI "IMAGENAME eq acro*"
+taskkill.exe /F /FI "IMAGENAME eq adobe*"
+taskkill.exe /F /FI "IMAGENAME eq adobe *"
+taskkill.exe /F /FI "IMAGENAME eq Adobe*"
+taskkill.exe /F /FI "IMAGENAME eq AdobeIPCBroker*"
+taskkill.exe /F /FI "IMAGENAME eq armsvc*"
+taskkill.exe /F /FI "IMAGENAME eq plugin-container*"
+taskkill.exe /F /FI "IMAGENAME eq AdobeCollabSync*"
+taskkill.exe /F /FI "IMAGENAME eq Creative*"
+taskkill.exe /F /FI "IMAGENAME eq Creative *"
+taskkill.exe /F /FI "IMAGENAME eq CCXProcess*"
+taskkill.exe /F /FI "IMAGENAME eq CCLibrary*"
+taskkill.exe /F /FI "IMAGENAME eq Core*"
+taskkill.exe /F /FI "IMAGENAME eq CCLibraries*"
+
+"C:\Program Files\Adobe\Adobe Creative Cloud\ACC\Creative Cloud.exe"
+
+exit
