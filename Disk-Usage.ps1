899a790e596a8a1abcd05184cb4277c13162fea2
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-08-31 12:34:18 +0100

    Create Disk-Usage.ps1

---
+++ Disk-Usage.ps1
@@ -0,0 +1,1 @@
+Get-CimInstance -Class CIM_LogicalDisk | Select-Object @{Name="Size(GB)";Expression={$_.size/1gb}}, @{Name="Free Space(GB)";Expression={$_.freespace/1gb}}, @{Name="Free (%)";Expression={"{0,6:P0}" -f(($_.freespace/1gb) / ($_.size/1gb))}}, DeviceID, DriveType | Where-Object DriveType -EQ '3' | Out-file -filepath c:\temp\diskreport.txt -Encoding ascii
