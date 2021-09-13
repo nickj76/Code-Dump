41c7fe29586658c9b13b96954f2e6f321aad9d58
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-13 10:30:31 +0100

    Update Custom-Printer-add.ps1

--- Mixed-Scripts/Custom-Printer-add.ps1
+++ Mixed-Scripts/Custom-Printer-add.ps1
@@ -1,4 +1,13 @@
-pnputil.exe /a "\\fileshare\HPPrinter\*.inf"
-Add-PrinterDriver -Name "HP OfficeJet 5200 series PCL-3" -InfPath "C:\Windows\System32\DriverStore\FileRepository\hpygid24_v4.inf_amd64_f312bf16a5228084\hpygid24_v4.inf"
-Add-PrinterPort -Name "Ports McGee" -PrinterHostAddress "IP Of Printer"
-Add-Printer -DriverName "HP OfficeJet 5200 series PCL-3" -Name "Printy" -PortName "Ports McGee"
+pnputil.exe /add-driver "C:\Temp\Printers\KOAXPJ__.INF" /install
+Add-PrinterDriver -Name "KONICA MINOLTA C3851SeriesPCL" 
+Add-PrinterPort -Name "\\winprint.surrey.ac.uk\SSPDutyOffice" -PrinterHostAddress "\\winprint.surrey.ac.uk\SSPDutyOffice"
+Add-Printer -DriverName "KONICA MINOLTA C3851SeriesPCL" -Name "SSPDutyOffice" -PortName "SSPDutyOffice"
+
+# Create Detection Method.
+$path = "C:\logfiles"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+New-Item -ItemType "file" -Path "c:\logfiles\SSPDutyOfficePrinter.txt"
