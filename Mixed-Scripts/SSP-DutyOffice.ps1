93805e978057ebebcf4f61298a56ff65fd1599e1
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-20 13:15:56 +0100

    Create SSP-DutyOffice.ps1

---
+++ Mixed-Scripts/SSP-DutyOffice.ps1
@@ -0,0 +1,13 @@
+pnputil.exe -i -a  C:\Temp\Printers\*.inf
+Add-PrinterDriver -Name "KONICA MINOLTA C3851SeriesPCL" 
+Add-PrinterPort -Name "\\winprint.surrey.ac.uk\SSPDutyOffice" -PrinterHostAddress "\\winprint.surrey.ac.uk\SSPDutyOffice"
+Add-Printer -DriverName "KONICA MINOLTA C3851SeriesPCL" -Name "SSPDutyOffice" -PortName "SSPDutyOffice"
+
+# Create Detection Method.
+$path = "C:\logfiles"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+New-Item -ItemType "file" -Path "c:\logfiles\SSPDutyOfficePrinter.txt"
