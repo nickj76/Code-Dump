9b8947477da7afb2a847cd93e5d5ef31d3c64482
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-23 12:29:15 +0100

    updating

--- Mixed-Scripts/Print-Fix2v3.ps1
+++ Mixed-Scripts/Print-Fix2v3.ps1
@@ -1,25 +1,19 @@
-$keypath = “HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint”
-$keypath2 = “HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PackagePointAndPrint”
-$keypath3= “HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\ListofServers”
-$keypath4 = “HKLM:\Software\Policies\Microsoft\Windows\DriverInstall\Restrictions\AllowUserDeviceClasses”
-$keypath5 = “HKLM:\Software\Policies\Microsoft\Windows\DriverInstall\Restrictions”
+New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -force | Out-Null
+New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PackagePointAndPrint' -force | Out-Null
+New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\ListofServers' -force | Out-Null
+New-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows\DriverInstall\Restrictions' -force | Out-Null
+New-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows\DriverInstall\Restrictions\AllowUserDeviceClasses' -force | Out-Null
 
-New-Item -Path $keyPath -force | Out-Null
-New-Item -Path $keyPath2 -force | Out-Null
-New-Item -Path $keyPath3 -force | Out-Null
-New-Item -Path $keypath5 -force | Out-Null
-New-Item -Path $keypath4 -force | Out-Null
+new-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -Name RestrictDriverInstallationToAdministrators -Value 0 -force
+new-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -Name TrustedServers -Value 1 -force
+new-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -Name Restricted -Value 0 -force
+new-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -Name NoWarningNoElevationOnInstall -Value 1 -force
+new-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -Name TrustedServers -Value 1 -force
+new-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -Name UpdatePromptSettings -Value 2 -force
+new-itemproperty -path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -Name Serverlist -type string -value winprint.surrey.ac.uk -force
 
-new-ItemProperty -Path $keypath -Name RestrictDriverInstallationToAdministrators -Value 0 -force
-new-ItemProperty -Path $keypath -Name TrustedServers -Value 1 -force
-new-ItemProperty -Path $keypath -Name Restricted -Value 0 -force
-new-ItemProperty -Path $keypath -Name NoWarningNoElevationOnInstall -Value 1 -force
-new-ItemProperty -Path $keypath -Name TrustedServers -Value 1 -force
-new-ItemProperty -Path $keypath -Name UpdatePromptSettings -Value 2 -force
-new-itemproperty -path $keyPath -Name Serverlist -type string -value winprint.surrey.ac.uk -force
-
-new-ItemProperty -Path $keypath2 -Name PackagePointAndPrintServerList -Value 1 -force
-new-ItemProperty -Path $keypath3 -Name winprint.surrey.ac.uk -Value winprint.surrey.ac.uk -force
+new-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PackagePointAndPrint' -Name PackagePointAndPrintServerList -Value 1 -force
+new-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\ListofServers' -Name winprint.surrey.ac.uk -Value winprint.surrey.ac.uk -force
 
 $name1 = “printer”
 $value1 = “{4658ee7e-f050-11d1-b6bd-00c04fa372a7}”
@@ -30,7 +24,7 @@ $value3 = 1
 $name4 = “LocalPrintQueue”
 $value4 = “{1ed2bbf9-11f0-4084-b21f-ad83a8e6dcdc}”
 
-New-ItemProperty -Path $keypath4 -Name $name3 -Value $value3 -PropertyType Dword | Out-Null
-New-ItemProperty -Path $keypath5 -Name $name1 -Value $value1 -PropertyType String | Out-Null
-New-ItemProperty -Path $keypath5 -Name $name2 -Value $value2 -PropertyType String | Out-Null
-New-ItemProperty -Path $keypath5 -Name $name4 -Value $value4 -PropertyType String | Out-Null
+New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\DriverInstall\Restrictions\AllowUserDeviceClasses' -Name $name3 -Value $value3 -PropertyType Dword | Out-Null
+New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\DriverInstall\Restrictions' -Name $name1 -Value $value1 -PropertyType String | Out-Null
+New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\DriverInstall\Restrictions' -Name $name2 -Value $value2 -PropertyType String | Out-Null
+New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\DriverInstall\Restrictions' -Name $name4 -Value $value4 -PropertyType String | Out-Null
