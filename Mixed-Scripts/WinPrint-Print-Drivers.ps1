619a08af65cc8a95a3557ac5f11ca8455ba55e88
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-24 11:13:21 +0100

    Update WinPrint-Print-Drivers.ps1

--- Mixed-Scripts/WinPrint-Print-Drivers.ps1
+++ Mixed-Scripts/WinPrint-Print-Drivers.ps1
@@ -1,26 +1,24 @@
 <# 
 .SYNOPSIS
-   Install Script to add the Surrey Print Driver to Windows.
+   Install Script to add the WinPrint Printer Drivers to Windows.
 
 .DESCRIPTION
-   Add the Samsung Universal Print Driver for SurreyPrint to the Windows Driver store and make avaliable for use. 
+   Add the various printer drivers installed on WinPrint to the Windows Driver store and make avaliable for use. 
 
 .EXAMPLE
-   PS C:\> .\Samsung-UniPrint-Driver.ps1
+   PS C:\> .\WinPrint-Print-Drivers.ps1
    Save the file to your hard drive with a .PS1 extention and run the file from an elavated PowerShell prompt.
 
 .FUNCTIONALITY
    PowerShell v1+
 #>
 
-Start-Transcript -Path c:\temp\SamsungDriver.txt
+Start-Transcript -Path c:\temp\WinPrintDrivers.txt
 
 &"C:\Program Files\7-Zip\7z.exe" x -oc:\temp\ -y *.7z
 
-Get-ChildItem "C:\temp\printer" -Recurse -Filter "*.inf" | 
-ForEach-Object { C:\Windows\Sysnative\PNPUtil.exe /add-driver $_.FullName /install }
-
-add-printerdriver -name 'Samsung Universal Print Driver 2 PCL6'
+Get-ChildItem "C:\temp\Winprint" -Recurse -Filter "*.inf" | 
+ForEach-Object { C:\Windows\Sysnative\PNPUtil.exe /add-driver $_.FullName /install /Subdirs }
 
 # Create Detection Method.
 $path = "C:\logfiles"
@@ -29,7 +27,7 @@ If(!(test-path $path))
       New-Item -ItemType Directory -Force -Path $path
 }
 
-New-Item -ItemType "file" -Path "c:\logfiles\SamsungUniPrintDriver.txt"
+New-Item -ItemType "file" -Path "c:\logfiles\WinPrint-Print-Driver.txt"
 Stop-Transcript
 
 
