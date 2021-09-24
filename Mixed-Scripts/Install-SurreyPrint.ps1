9ba20baa231cf605b7f1a819223cdee2fba98f96
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-24 16:50:03 +0100

    Adding

---
+++ Mixed-Scripts/Install-SurreyPrint.ps1
@@ -0,0 +1,36 @@
+<# 
+.SYNOPSIS
+   Install Script to add the SurreyPrint Printer Queue to the local machine.
+
+.DESCRIPTION
+   Add the SurreyPrint Printer Queue and make avaliable for use. 
+
+.EXAMPLE
+   PS C:\> .\SurreyPrintQueue.ps1
+   Save the file to your hard drive with a .PS1 extention and run the file from an elavated PowerShell prompt.
+
+.FUNCTIONALITY
+   PowerShell v1+
+#>
+
+Start-Transcript -Path c:\temp\SurreyPrint.txt
+
+$printServer = "printservice.surrey.ac.uk"
+$printer = "SurreyPrint"
+
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\$printServer\$printer }
+
+## Pause for 45 seconds to allow install to completed.
+Start-Sleep -Seconds 30
+
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\$printServer\$printer }
+
+# Create Detection Method.
+$path = "C:\logfiles"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+New-Item -ItemType "file" -Path "c:\logfiles\SurreyPrint.txt"
+Stop-Transcript
