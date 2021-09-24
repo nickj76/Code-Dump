a2c5cd33281b4b510a3ea98d3da350581942e68d
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-24 17:23:33 +0100

    Update Add-SurreyPrint.ps1

--- Mixed-Scripts/Add-SurreyPrint.ps1
+++ Mixed-Scripts/Add-SurreyPrint.ps1
@@ -1,4 +1,19 @@
+<# 
+.SYNOPSIS
+   Install Script to add the SurreyPrint Printer Queue to the local machine.
 
+.DESCRIPTION
+   Add the SurreyPrint Printer Queue and make avaliable for use. 
+
+.EXAMPLE
+   PS C:\> .\SurreyPrintQueue.ps1
+   Save the file to your hard drive with a .PS1 extention and run the file from an elavated PowerShell prompt.
+
+.FUNCTIONALITY
+   PowerShell v1+
+#>
+
+Start-Transcript -Path c:\temp\SurreyPrintQ.txt
 
 $Printer = "\\printservice.surrey.ac.uk\SurreyPrint"
 
@@ -9,3 +24,13 @@ try {
 Catch [System.Exception] {
   Write-Host "Error adding printer $($Printer) with error $($_.Exception.Message)"
 }
+
+# Create Detection Method.
+$path = "C:\logfiles"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+New-Item -ItemType "file" -Path "c:\logfiles\SurreyPrintQ.txt"
+Stop-Transcript
