8b73931575af585c42d653557ede3bddaa3c07f1
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-24 17:10:25 +0100

    Update Install-SSPAdminOffice-Printer.ps1

--- Mixed-Scripts/Install-SSPAdminOffice-Printer.ps1
+++ Mixed-Scripts/Install-SSPAdminOffice-Printer.ps1
@@ -1,12 +1,12 @@
 <# 
 .SYNOPSIS
-   Install Script to add the SurreyPrint Printer Queue to the local machine.
+   Install Script to add the SSP Printer Queues to the local machine.
 
 .DESCRIPTION
-   Add the SurreyPrint Printer Queue and make avaliable for use. 
+   Add the SSP Printer Queues and make avaliable for use. 
 
 .EXAMPLE
-   PS C:\> .\SurreyPrintQueue.ps1
+   PS C:\> .\SSPPrintQueues.ps1
    Save the file to your hard drive with a .PS1 extention and run the file from an elavated PowerShell prompt.
 
 .FUNCTIONALITY
@@ -17,13 +17,14 @@ Start-Transcript -Path c:\temp\SSPAdminOffice.txt
 
 $printServer = "Winprint.surrey.ac.uk"
 $printer = "SSPAdminOffice"
+$printer1 = "SSPDutyOffice"
+$printer2 = "Q_SSP_10"
 
 Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\$printServer\$printer }
 
-## Pause for 45 seconds to allow install to completed.
-Start-Sleep -Seconds 30
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\$printServer\$printer1 }
 
-Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\$printServer\$printer }
+Invoke-Command -ScriptBlock { rundll32 printui.dll, PrintUIEntry /in /n\\$printServer\$printer2 }
 
 # Create Detection Method.
 $path = "C:\logfiles"
