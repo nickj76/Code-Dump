a84de4f99eef4ac75f25cfa5da96f008cd1dd025
nickj76 <40469127+nickj76@users.noreply.github.com>
2021-09-24 08:30:00 +0100

    updating

--- Mixed-Scripts/Samsung-driver-v4.ps1
+++ Mixed-Scripts/Samsung-driver-v4.ps1
@@ -2,7 +2,7 @@ Start-Transcript -Path c:\temp\SamsungDriver.txt
 &"C:\Program Files\7-Zip\7z.exe" x -oc:\temp\ -y *.7z
 
 Get-ChildItem "C:\temp\printer" -Recurse -Filter "*.inf" | 
-ForEach-Object { C:\Windows\SysNative\PNPUtil.exe /add-driver $_.FullName /install }
+ForEach-Object { %windir%\SysNative\PNPUtil.exe /add-driver $_.FullName /install }
 
 add-printerdriver -name 'Samsung Universal Print Driver 2 PCL6'
 
