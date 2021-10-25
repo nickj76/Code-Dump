2d6ef2f59817a896bd0bb0a34b5a4a147d880bb0
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2021-10-25 11:28:22 +0100

    Create Check_Windows_hostname.ps1

---
+++ Proactive-Remediations-WIP/Notify-name-change/Check_Windows_hostname.ps1
@@ -0,0 +1,26 @@
+
+Try
+{
+    $computername = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+
+    if ($computername -eq "UWS*")
+    {
+        #Exit 0 Machine Name Correct.
+        Write-Host "All Ok"            
+        exit 0
+    }
+    else 
+    {
+        #Exit 1 has been renamed
+        Write-Host "Machine has been Renamed"
+        exit 1        
+    }
+
+}
+
+catch
+{
+    $errMsg = $_.Exception.Message
+    return $errMsg
+    exit 1
+}
