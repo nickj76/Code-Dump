0bc6c0eb4c5af971635da4be24d697d68f41b86f
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-03-24 10:16:27 +0000

    updated

--- Base64/Convert-txt-Base64.ps1
+++ Base64/Convert-txt-Base64.ps1
@@ -1,3 +1,8 @@
-$MYTEXT = 'insert your text here'
+$MYTEXT = '<Configuration>
+<!--Uninstall complete Office 365-->
+<Display Level="None" AcceptEULA="TRUE" />
+<Logging Level="Standard" Path="%temp%" />
+<Remove All="TRUE" />
+</Configuration>'
 $ENCODED = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($MYTEXT))
 Write-Output $ENCODED
