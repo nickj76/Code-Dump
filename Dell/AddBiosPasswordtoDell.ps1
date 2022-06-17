38914b9a69bc5125e390f2f88dbfb9166b8eb535
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-06-17 10:53:18 +0100

    updated

--- Dell/AddBiosPasswordtoDell.ps1
+++ Dell/AddBiosPasswordtoDell.ps1
@@ -8,6 +8,6 @@ $VcList | Install-VcRedist -Path C:\Temp\VcRedist
 Install-Module -Name DellBIOSProvider -Force
 Import-Module -Name DellBIOSProvider
 
-$x = "bDtja2Q0d24="
+$x = "Base64 String here"
 $z = [System.Text.Encoding]::Ascii.GetString([System.Convert]::FromBase64String($x));Set-Item -Path DellSmbios:\Security\AdminPassword $z
 
