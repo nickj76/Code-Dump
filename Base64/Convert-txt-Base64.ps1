665f3f766e6656631f69f36373f2dac0ca079be9
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-07-22 08:47:01 +0100

    updated

--- Base64/Convert-txt-Base64.ps1
+++ Base64/Convert-txt-Base64.ps1
@@ -1,3 +1,41 @@
-$MYTEXT = 'insert your text here'
+$MYTEXT = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+
+<Activation>
+
+  <Request>
+
+    <FirstName>Ron</FirstName>
+
+    <LastName>Marsh</LastName>
+
+    <Email>r.a.marsh@surrey.ac.uk</Email>
+
+    <Phone>+1-555-555-5555</Phone>
+
+    <Fax>+1-555-555-5555</Fax>
+
+    <JobTitle>Your Job Title</JobTitle>
+
+    <Industry>Your Industry</Industry>
+
+    <Department>Your Department</Department>
+
+    <Organization>University of Surrey</Organization>
+
+    <AddressLine1>Stag Hill Campus</AddressLine1>
+
+    <AddressLine2>Austin Pearce Building</AddressLine2>
+
+    <City>Guildford</City>
+
+    <Zipcode>GU27XH</Zipcode>
+
+    <Country>UK</Country>
+
+    <State>Surrey</State>
+
+  </Request>
+
+</Activation>'
 $ENCODED = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($MYTEXT))
 Write-Output $ENCODED
