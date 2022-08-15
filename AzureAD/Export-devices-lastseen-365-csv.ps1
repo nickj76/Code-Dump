ce528ba6a5a1942201fe0f7c2f1afdedeb219f7f
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-08-15 12:06:03 +0100

    created and updated

---
+++ AzureAD/Export-devices-lastseen-365-csv.ps1
@@ -0,0 +1,29 @@
+<#  .SYNOPSIS
+    Powershell Script to find all computers in Intune and when the last time they were online.
+
+    .NOTES
+    filename: Get-old-devices.ps1
+    
+    If computer not used in 90 day export list to CSV located: export-csv C:\IntuneFiles\devicelist-olderthan-90days-summary.csv
+    Change line 28 as required e.g. .AddDays(-90) or .AddDays(-60)     
+    #>
+    
+#Sign-in to AzureAD
+Connect-AzureAD
+
+#check if Folder exists
+$folderName = "IntuneFiles"
+$Path="C:\"+$folderName
+
+if (!(Test-Path $Path))
+{
+New-Item -itemType Directory -Path C:\ -Name $FolderName
+}
+else
+{
+write-host "Folder already exists"
+}
+
+#Get CSV to Desktop of all Devices not used in 90 Days
+$dt = (Get-Date).AddDays(-365)
+Get-AzureADDevice -All:$true | Where-Object {$_.ApproximateLastLogonTimeStamp -le $dt} | select-object -Property AccountEnabled, DeviceId, DeviceOSType, DeviceOSVersion, DisplayName, DeviceTrustType, ApproximateLastLogonTimestamp | export-csv C:\IntuneFiles\devicelist-olderthan-365days-summary.csv -NoTypeInformation
--- AzureAD/disable-remove-devices-intune.ps1
+++ AzureAD/disable-remove-devices-intune.ps1
@@ -1,10 +1,14 @@
-<#  .SYNOPSIS
+<#  
+    .SYNOPSIS
     Powershell Script to find all computers in Intune and when the last time they were online.
+    
     .NOTES
-    filename: Get-old-devices.ps1
+    filename: Manage-Intune-Device-Older-365days.ps1
+    
     Powershell Script to find all computers in Intune and when the last time they were online.
-    If not used in last 90 days to remove them.
-    #>
+    If not older used in last 365 days to remove them.
+    
+#>
     
 #Sign-in to AzureAD
 Connect-AzureAD
@@ -22,37 +26,33 @@ else
 write-host "Folder already exists"
 }
 
-#Get CSV to Desktop of all Devices not used in 90 Days
-$dt = (Get-Date).AddDays(-90)
-Get-AzureADDevice -All:$true | Where-Object {$_.ApproximateLastLogonTimeStamp -le $dt} | select-object -Property AccountEnabled, DeviceId, DeviceOSType, DeviceOSVersion, DisplayName, DeviceTrustType, ApproximateLastLogonTimestamp | export-csv C:\IntuneFiles\devicelist-olderthan-90days-summary.csv -NoTypeInformation
+#Get CSV to Desktop of all Devices not used in 365 Days
+$dt = (Get-Date).AddDays(-365)
+Get-AzureADDevice -All:$true | Where-Object {$_.ApproximateLastLogonTimeStamp -le $dt} | select-object -Property AccountEnabled, DeviceId, DeviceOSType, DeviceOSVersion, DisplayName, DeviceTrustType, ApproximateLastLogonTimestamp | export-csv C:\IntuneFiles\devicelist-olderthan-365days-summary.csv -NoTypeInformation
 
-
-<##
-#Get Disable all Devices not used in 90 Days
+#Get Disable all Devices not used in 365 Days
 $disyesorno = Read-Host -Prompt "Would you like to disable all devices (Y/N)?"
 
 if($disyesorno -eq "Y") {
-        $dt = (Get-Date).AddDays(-90)
+        $dt = (Get-Date).AddDays(-365)
         Get-AzureADDevice -All:$true | Where-Object {$_.ApproximateLastLogonTimeStamp -le $dt} | Set-AzureADDevice -AccountEnabled $false
-        write-host("All Devices older than 90 Days have been disabled!")
+        write-host("All Devices older than 365 Days have been disabled!")
     }else{
       write-host("No Devices have been disabled!")
 }
 
-#Remove all Devices not used in 90 Days
+#Remove all Devices not used in 365 Days
 $rmvyesorno = Read-Host -Prompt "Would you like to remove all devices (Y/N)?"
 
 if($rmvyesorno -eq "Y") {
-        $dt = (Get-Date).AddDays(-90)
+        $dt = (Get-Date).AddDays(-365)
         $Devices = Get-AzureADDevice -All:$true | Where-Object {($_.ApproximateLastLogonTimeStamp -le $dt) -and ($_.AccountEnabled -eq $false)}
             foreach ($Device in $Devices) {
             Remove-AzureADDevice -ObjectId $Device.ObjectId
         }
-        write-host("All Devices older than 90 Days have been removed!")
+        write-host("All Devices older than 365 Days have been removed!")
     }else{
       write-host("No Devices have been removed!")
 }
 
 pause
-
-##>
