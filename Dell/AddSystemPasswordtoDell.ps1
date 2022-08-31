e5445c3bccae1b75c966ee05b4c07291c20ace85
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-08-31 11:25:52 +0100

    created

---
+++ Dell/AddSystemPasswordtoDell.ps1
@@ -0,0 +1,13 @@
+Install-PackageProvider -Name NuGet -Force
+
+Install-Module VcRedist
+Import-Module VcRedist
+$VcList = Get-VcList | Get-VcRedist -Path "C:\Temp\VcRedist"
+$VcList | Install-VcRedist -Path C:\Temp\VcRedist
+
+Install-Module -Name DellBIOSProvider -Force
+Import-Module -Name DellBIOSProvider
+
+$x = "Base64 String here"
+$z = [System.Text.Encoding]::Ascii.GetString([System.Convert]::FromBase64String($x));Set-Item -Path DellSmbios:\Security\SystemPassword $z
+
