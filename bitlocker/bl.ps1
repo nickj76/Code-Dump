8f1e6081a2bc8ec101086924a3b692eb068a9e88
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-09-06 17:32:27 +0100

    updated

--- bitlocker/bl.ps1
+++ bitlocker/bl.ps1
@@ -1,3 +1,20 @@
 $TpmProtectorID = ((Get-BitLockerVolume -MountPoint c).KeyProtector | Where-Object KeyProtectorType -EQ 'Tpm').KeyProtectorID
 Remove-BitLockerKeyProtector -MountPoint c -KeyProtectorId $TpmProtectorID
-Restart-Computer -Force
+Stop-Computer -Force
+
+## Create Detection Method. 
+$logfilespath = "C:\logfiles"
+If(!(test-path $logfilespath))
+{
+      New-Item -ItemType Directory -Force -Path $logfilespath
+}
+
+New-Item -ItemType "file" -Path "c:\logfiles\Missing-Device.txt"
+
+
+
+
+<# 
+Add-BitLockerKeyProtector -MountPoint c -TpmProtector
+Restart-Computer -Force 
+#>
