033f8643d0ce89ffa0744e06ccc7b18a2896542d
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-10-06 17:04:15 +0100

    created

---
+++ Office/Detect-Office-Build.ps1
@@ -0,0 +1,20 @@
+# Define paths where Office could be located
+$paths = @(
+    "HKLM:\SOFTWARE\Microsoft\Office\ClickToRun",
+    "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Office\ClickToRun"
+)
+
+# Variable to hold version
+$officeVersion = ""
+
+# Loop through paths and find version
+foreach ($path in $paths) {
+    # Check to see if path exists
+    if (Test-Path -Path "$path\Configuration") {
+        $officeVersion = (Get-ItemProperty -Path "$path\Configuration" -Name "VersionToReport").VersionToReport
+    }
+}
+
+Write-Host $officeVersion
+
+
