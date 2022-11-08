dfcc0725702988be0a45e03da77adc6fa50bf84e
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2022-11-08 13:13:58 +0000

    removed

--- Dell/Install-DCU.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item -Path "C:\Program Files\Dell\CommandUpdate" -Recurse -Force -EA SilentlyContinue -Verbose
-Remove-Item -Path "C:\Program Files (x86)\Dell\CommandUpdate" -Recurse -Force -EA SilentlyContinue -Verbose
-.\Dell-Command-Update-Application_T97XP_WIN_4.6.0_A00.EXE
