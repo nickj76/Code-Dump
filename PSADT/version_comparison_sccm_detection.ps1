e6dec7ee3740b1759b2a5d9686c3d488454abcbf
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2023-03-24 20:45:52 +0000

    Deleted

--- PSADT/version_comparison_sccm_detection.ps1
+++
@@ -1,3 +0,0 @@
-        $expected_version = [version]"4.6.2"
-
-        If( (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Version -ge $expected_version) { Write-Host "Installed" } 
