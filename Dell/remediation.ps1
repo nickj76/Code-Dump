e1f7583f7e6621d975570e081f3b51ac5c8d0593
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2023-06-14 11:23:00 +0100

    created

--- Dell/remediation.ps1
+++
@@ -1,2 +0,0 @@
-$DownloadLocation = "C:\Program Files\Dell\CommandUpdate"
-start-process "$($DownloadLocation)\dcu-cli.exe" -ArgumentList "/applyUpdates -autoSuspendBitLocker=disable -reboot=disable" -Wait
