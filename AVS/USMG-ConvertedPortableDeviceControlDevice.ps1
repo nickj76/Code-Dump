1518d3aa8d96fcf2aa78fbec3288cfa6a63a02dd
Nick Jenkins <40469127+nickj76@users.noreply.github.com>
2023-06-14 11:19:56 +0100

    updated

--- AVS/USMG-ConvertedPortableDeviceControlDevice.ps1
+++
@@ -1,1 +0,0 @@
-Set-CimInstance -Query 'SELECT * FROM MSPower_DeviceEnable WHERE InstanceName LIKE "BUTTONCONVERTER\\%"' -Namespace root/WMI -Property @{Enable = $false}
--- Windows-PowerSettings/README.md
+++
@@ -1,6 +0,0 @@
-# Windows_Power_Settings
-
-Disable Hibernate
-Disable Sleep
-
-create a scheduled shutdown at 10pm every day, checks to see if a user is logged in.
